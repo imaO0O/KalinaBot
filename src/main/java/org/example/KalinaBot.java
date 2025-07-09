8901154de338af4e9e13fd08d5063e9b05b82f2b
Svetlana Tsykunova <tsykunova.svetlana05@gmail.com>
2025-07-09 20:26:47 +0300

    версия 1.0

--- src/main/java/org/example/KalinaBot.java
+++ src/main/java/org/example/KalinaBot.java
@@ -8,16 +8,22 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class KalinaBot extends TelegramLongPollingBot {
 
     //словарь с айди и датой
     private final Map<Long, LocalDate> lastStickerDate = new HashMap<>();
-
+    private final List<String> stickerIds = loadStickerIdsFromJson();
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage()) {
@@ -29,7 +35,9 @@ public class KalinaBot extends TelegramLongPollingBot {
                 // запуск калины бедолаге
                 SendSticker sticker = new SendSticker();
                 sticker.setChatId(userId);
-                sticker.setSticker(new InputFile("CAACAgIAAxkBAAEBQ2xkheMuY5I1fHhqU1xz5cYxFY8HwAACRgADVp29CqHETpoHRi3hNAQ"));
+                String randomId = stickerIds.get((int)(Math.random()*stickerIds.size()));
+
+                sticker.setSticker(new InputFile(randomId));
 
 
                 SendMessage text = new SendMessage();
@@ -59,13 +67,30 @@ public class KalinaBot extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
-        return "YourKalinaBot"; // Имя бота
+        return "KalinaBot"; // Имя бота
     }
 
     @Override
     public String getBotToken() {
         return "7789374063:AAHVT5SGQFdUxAQbHO-VaYzYxDXEKeV_upE";
     }
+
+    private List<String> loadStickerIdsFromJson() {
+        ObjectMapper mapper = new ObjectMapper();
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream("stickers.json")) {
+            JsonNode root = mapper.readTree(is);
+            JsonNode stickersArray = root.path("stickers");
+            List<String> ids = new ArrayList<>();
+            for (JsonNode node : stickersArray) {
+                ids.add(node.get("file_id").asText());
+            }
+            return ids;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
+
 
     public static void main(String[] args) {
         try {
